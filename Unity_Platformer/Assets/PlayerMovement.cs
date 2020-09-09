5bfdf554dc72e3766956db6543e10e534c195bca
Thunder19 <braydenhamous@gmail.com>
2020-09-09 13:59:34 -0500

    Revert "Third"
    This reverts commit 5453111387ab91c181da2635e2532b31acc8d549.

--- Unity_Platformer/Assets/PlayerMovement.cs
+++ Unity_Platformer/Assets/PlayerMovement.cs
@@ -6,16 +6,10 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
     float moveSpeed = 1;
-
-    [SerializeField]
-    float jumpSpeed = 1;
-
-    Rigidbody rb;
-
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
@@ -24,20 +18,8 @@ public class PlayerMovement : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
-        rb.velocity = new Vector3(h * moveSpeed,
-                                  rb.velocity.y,
-                                  v * moveSpeed);
-
-        if(Input.GetButtonDown("Jump"))
-        {
-            Jump();
-        }
-    }
-
-    void Jump()
-    {
-        rb.velocity = new Vector3(rb.velocity.x,
-                                   jumpSpeed,
-                                   rb.velocity.z);
+        transform.position = new Vector3(transform.position.x + h * Time.deltaTime * moveSpeed,
+                                         transform.position.y,
+                                         transform.position.z + v * Time.deltaTime * moveSpeed);
     }
 }
